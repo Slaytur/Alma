d6785121de8332026f95c4af6f4276ba876f1a9e
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 13:16:54 -0500

    8====>

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import LogoImg from '../assets/img/favicons/favicon.png';
+import LogoImg from '../assets/img/favicons/Almsa.png';
 
 class Logo extends React.Component {
     render = (): React.ReactNode => (
-        <img className="tw-w-16 tw-h-16" src={LogoImg} />
+        <img className="tw-w-32 tw-h-16" src={LogoImg} />
     );
 }
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
