69517cb689988fcfd36dad8fab0c29e4fd2b45d3
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 16:36:30 -0500

    FIX CLASS CODE

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-class About extends React.Component
-{
-    render = () : React.ReactNode => (
+class About extends React.Component {
+    render = (): React.ReactNode => (
         <div className="tw-text-center tw-outline-10">
             <div className="tw-text-[50px]"> <h1><b> Alma </b></h1> </div>
             <br/>
@@ -11,8 +10,8 @@ class About extends React.Component
                 <li>Caterred to Students</li> <br/>
                 <li>By Students</li>
             </ol>
-        </div> 
-    )
+        </div>
+    );
 }
 
 export default About;
--- client/src/pages/JoinClass.tsx
+++ client/src/pages/JoinClass.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 class JoinClass extends React.Component {
-    render = (): React.ReactNode =>
-        (
-            <div className="tw-relative tw-top-40 tw-text-center tw-item-center tw-bg-grey">
-                <form className="tw-bg-primary tw-mx-[35rem] tw-py-25 tw-rounded-lg">
-                    <h1 className="tw-text-xl tw-py-1"> Class Code</h1>
-                    <input type="text" name="classcode"/> <br/>
-                    <input className="tw-py-1" type="submit"/>
+    render = (): React.ReactNode => (
+        <main className="container tw-mt-10">
+            <h1 className="text-center my-5 tw-text-6xl">Join Class</h1>
+            <div className="card card-body text-dark tw-bg-[#b1cbe6]">
+                <form action="/" method="GET">
+                    <div className="form-group mb-3">
+                        <label htmlFor="username" className="form-label">Class Code</label>
+                        <input type="username" name="class-code" className="form-control mb-3" placeholder="Enter your class code..." maxLength={7} required />
+                    </div>
                 </form>
             </div>
-        );
+        </main>
+    );
 }
 
 export default JoinClass;
