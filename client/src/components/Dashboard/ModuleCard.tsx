ed7a857fa4693fb28e5dbc04580707d4ecb6e0a0
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 15:10:38 -0500

    Update ModuleCard.tsx

--- client/src/components/Dashboard/ModuleCard.tsx
+++ client/src/components/Dashboard/ModuleCard.tsx
@@ -18,6 +18,10 @@ class ModuleCard extends React.Component<Record<string, never>, { state: `COLLAP
         console.log(`show the lecture element with right materials`);
     };
 
+    opensesamea = (): void => {
+        console.log(`show the lecture element with right materials`);
+    };
+
     render = (): React.ReactNode => (
         <section className=" tw-font-poppins tw-font-normal tw-ml-4 tw-w-[90%] md:tw-w-2/4">
             <div onClick={this.changeState} className="card tw-cursor-pointer tw-select-none tw-mt-2 tw-bg-primary tw-bg-opacity-60 tw-text-[26px] tw-h-16 tw-justify-left tw-items-start tw-rounded-md tw-w-full tw-flex tw-justify-start">
@@ -26,9 +30,11 @@ class ModuleCard extends React.Component<Record<string, never>, { state: `COLLAP
                     <h1 className="tw-w-fit">Module - X</h1>
                 </div>
             </div>
-            <div className={`card tw-justify-center tw-mb-2 tw-flex tw-bg-primary tw-bg-opacity-40 tw-text-[16px] tw-h-10 tw-mt-1 tw-justify-left tw-items-center tw-rounded-lg tw-w-5/6 ${this.state.state === `COLLAPSED` ? `tw-invisible` : ` tw-block`}`}>
-                <button onClick={this.view} className="tw-pl-6 tw-pr-16"></button>
-                <h1 className="tw-w-fit">Module - X.x</h1>
+            <div onClick={this.view} className={`tw-cursor-pointer tw-select-none card tw-justify-center tw-mb-2 tw-flex tw-bg-primary tw-bg-opacity-40 tw-text-[16px] tw-h-10 tw-mt-1 tw-justify-left tw-items-start tw-rounded-lg tw-w-5/6 ${this.state.state === `COLLAPSED` ? `tw-invisible` : ` tw-block`}`}>
+                <div className="tw-flex tw-ml-6">
+                    <i className="icofont icofont-rounded-right tw-w-fit"></i>
+                    <h1 className="tw-w-fit">Module - X.x</h1>
+                </div>
             </div>
         </section>
     );
