c4dbbdcebb4a12572eb22b3d0aeffa1da4b7e775
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 01:18:14 -0500

    reorganize sidebar
    Co-Authored-By: bennlol <52867429+bennlol@users.noreply.github.com>

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 class Sidebar extends React.Component {
     render = (): React.ReactNode => (
-        <section className="tw-mt-4 tw-ml-4 tw-w-32 tw-font-poppins tw-h-full tw-text-[rgba(255,255,255,0.9)]">
-            <div className="tw-text-black tw-bg-opacity-50 tw-rounded-md tw-py-2 tw-w-32 hover:tw-w-64 tw-ease-in tw-duration-200 tw-z-10 tw-bg-primary tw-justify-center tw-items-center tw-text-center mt-16 tw-h-full">
-                <h2 className="tw-text-xl">Classes</h2>
-                <hr className="tw-mx-4" />
-            </div>
+        <section className="tw-font-poppins tw-h-full tw-text-black tw-bg-opacity-40 tw-rounded-md tw-mt-4 tw-ml-4 tw-px-2 tw-py-3 tw-w-32 hover:tw-w-64 tw-ease-in tw-duration-150 tw-z-10 tw-bg-primary tw-text-center mt-16">
+            <h2 className="tw-text-xl">Classes</h2>
+            <hr className="tw-mx-4" />
+
+            <div className="tw-h-full tw-flex tw-flex-col tw-items-center"></div>
         </section>
     );
 }
