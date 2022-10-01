64cf4481b01fc14e9ebc6c5c7adebd285416ec50
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 00:58:35 -0500

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 class Sidebar extends React.Component {
     render = (): React.ReactNode => (
         <section className=" tw-m-2 tw-w-32 tw-font-poppins tw-text-2xl tw-h-full tw-text-[rgba(255,255,255,0.9)]">
-            <div className="tw-text-black tw-bg-opacity-50 tw-rounded-md tw-py-2 tw-w-32 hover:tw-w-64 tw-z-10 tw-bg-primary tw-justify-center tw-items-center tw-text-center mt-16 tw-h-full">
+            <div className="tw-text-black tw-bg-opacity-40 tw-rounded-md tw-py-2 tw-w-32 hover:tw-w-64 tw-z-10 tw-bg-primary tw-items-center tw-text-center mt-16 tw-h-full tw-flex tw-flex-col">
                 <h1>Classes</h1>
+                <div className= "tw-m-2 tw-h-1 tw-w-1/2 tw-bg-black"></div>
             </div>
         </section>
     );
