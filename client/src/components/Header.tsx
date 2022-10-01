8996c66eb065733123c074369aee6871942f2325
Sieradni <michaelandbrother@gmail.com>
2022-10-01 11:15:13 -0500

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -17,7 +17,7 @@ class Header extends React.Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-5 ">
+                        <ul className="navbar-nav ms-4 ">
                             <li className="nav-item"><a href="/" className="nav-link active" aria-current="page">Home</a></li>
                             <li className="nav-item"><a href="#" className="nav-link" aria-current="page">Link</a></li>
                             <li className="nav-item dropdown">
