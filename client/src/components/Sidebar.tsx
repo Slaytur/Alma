5d281c8f90151184b312b19bd7bb471612f7ecf9
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 11:23:41 -0500

    side patch

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 class Sidebar extends React.Component {
     render = (): React.ReactNode => (
-        <aside className="tw-w-32">
-            <div className='tw-w-32 hover:tw-w-64 tw-ease-in tw-duration-150 tw-z-10'>
+        <aside className="tw-w-32 md:tw-w-64 tw-z-10 tw-    bg-white">
+            <div className='tw-w-32 md:tw-w-64 hover:tw-w-64 tw-ease-in tw-duration-150 tw-z-10'>
                 <div className="tw-font-poppins tw-h-full tw-text-black tw-bg-opacity-40 tw-rounded-md tw-mt-4 tw-ml-4 tw-px-2 tw-py-3  tw-ease-in tw-duration-150 tw-z-10 tw-bg-primary tw-text-center mt-16">
                     <h2 className="tw-text-xl">Classes</h2>
                     <hr className="tw-mx-4" />
