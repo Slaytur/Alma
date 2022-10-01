07581ebd0528a5d141134ce0ed7ed74d355379d7
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 12:26:57 -0500

    Update ModuleCard.tsx

--- client/src/components/Dashboard/ModuleCard.tsx
+++ client/src/components/Dashboard/ModuleCard.tsx
@@ -22,8 +22,8 @@ class ModuleCard extends React.Component<Record<string, never>, { state: `COLLAP
         <section className=" tw-font-poppins tw-font-normal tw-ml-4 tw-w-[90%] md:tw-w-2/4">
             <div onClick={this.changeState} className="card tw-cursor-pointer tw-select-none tw-mt-2 tw-bg-primary tw-bg-opacity-60 tw-text-[26px] tw-h-16 tw-justify-left tw-items-start tw-rounded-md tw-w-full tw-flex tw-justify-start">
                 <div className='card-body tw-flex'>
-                    <i className={`icofont ${this.state.state === `COLLAPSED` ? `icofont-rounded-right` : `icofont-rounded-down`}  tw-pr-5`}></i>
-                    <h1 className="tw-w-fit tw-float-none">Module - X</h1>
+                    <i className={`icofont ${this.state.state === `COLLAPSED` ? `icofont-rounded-right` : `icofont-rounded-down`}  tw-pt-[2px] tw-pr-5`}></i>
+                    <h1 className="tw-w-fit">Module - X</h1>
                 </div>
             </div>
             <div className={`card tw-justify-center tw-mb-2 tw-flex tw-bg-primary tw-bg-opacity-40 tw-text-[16px] tw-h-10 tw-mt-1 tw-justify-left tw-items-center tw-rounded-lg tw-w-5/6 ${this.state.state === `COLLAPSED` ? `tw-invisible` : ` tw-block`}`}>
