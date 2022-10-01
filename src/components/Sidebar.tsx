81224a8da5e01d2c6f4e2b3bad1b3bfa56f5caad
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 01:09:35 -0500

    change to hr
    Co-Authored-By: bennlol <52867429+bennlol@users.noreply.github.com>

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ class Sidebar extends React.Component {
         <section className="tw-mt-4 tw-ml-4 tw-w-32 tw-font-poppins tw-h-full tw-text-[rgba(255,255,255,0.9)]">
             <div className="tw-text-black tw-bg-opacity-50 tw-rounded-md tw-py-2 tw-w-32 hover:tw-w-64 tw-ease-in tw-duration-200 tw-z-10 tw-bg-primary tw-justify-center tw-items-center tw-text-center mt-16 tw-h-full">
                 <h2 className="tw-text-xl">Classes</h2>
-                <div className="tw-m-2 tw-h-1 tw-w-1/2 tw-bg-black"></div>
+                <hr className="tw-mx-4" />
             </div>
         </section>
     );
