64d903281a0043ea77d0730c8f34e2524596e6c2
charlesbae <90109397+charlesbae@users.noreply.github.com>
2022-10-01 09:43:41 -0500

    note to all

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Logo from './Logo';
 
+// be able to query in DB !!!!
+
 class Header extends React.Component {
     render = (): React.ReactNode => (
         <header>
