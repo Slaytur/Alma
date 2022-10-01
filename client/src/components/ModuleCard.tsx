363f4631b5e092c4cdc2b061ad96fede6fcebf33
bennlol <52867429+bennlol@users.noreply.github.com>
2022-10-01 09:22:22 -0500

    height

--- client/src/components/ModuleCard.tsx
+++ client/src/components/ModuleCard.tsx
@@ -14,15 +14,19 @@ class ModuleCard extends React.Component<Record<string, never>, { state: `COLLAP
         this.setState({ state: this.state.state === `COLLAPSED` ? `EXPANDED` : `COLLAPSED` });
     };
 
+    view = (): void => {
+        console.log(`yes`);
+    };
+
     render = (): React.ReactNode => (
         <section className="tw-w-2/4">
-            <div className="tw-mt-2 tw-flex tw-bg-primary tw-bg-opacity-60 tw-text-[26px] tw-h-16 tw-justify-center tw-flex-col tw-items-center tw-rounded-md tw-w-full">
-                <h1 className="tw-w-fit">Module - X</h1>
-                <button onClick={this.changeState}>{this.state.state === `EXPANDED` ? `Collapse` : `Expand`}</button>
+            <div className="tw-mt-2 tw-bg-primary tw-bg-opacity-60 tw-text-[26px] tw-h-16 tw-justify-left tw-items-center tw-rounded-md tw-w-full tw-flex">
+                <button className="tw-pr-10 tw-pl-6" onClick={this.changeState}>{this.state.state === `EXPANDED` ? `Collapse` : `Expand`}</button>
+                <h1 className="tw-w-fit tw-float-none">Module - X</h1>
             </div>
-            <div className="tw-mb-2 tw-flex tw-bg-primary tw-bg-opacity-40 tw-text-[16px] tw-h-16 tw-justify-center tw-flex-col tw-items-center tw-rounded-md tw-w-5/6">
+            <div className={`tw-mb-2 tw-flex tw-bg-primary tw-bg-opacity-40 tw-text-[16px] tw-h-16 tw-justify-center tw-flex-col tw-items-center tw-rounded-md tw-w-5/6 ${this.state.state === `COLLAPSED` ? `tw-invisible` : ` tw-block`}`}>
+                <button onClick={this.view}>View</button>
                 <h1 className="tw-w-fit">Module - X.x</h1>
-                <button onClick={this.changeState}>{this.state.state === `EXPANDED` ? `Collapse` : `Expand`}</button>
             </div>
         </section>
     );
