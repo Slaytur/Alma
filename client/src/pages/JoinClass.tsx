086edb3f060d3d5fe88b619ef6235379e1e90510
charlesbae <90109397+charlesbae@users.noreply.github.com>
2022-10-01 13:55:33 -0500

    joincklass style

--- client/src/pages/JoinClass.tsx
+++ client/src/pages/JoinClass.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 class JoinClass extends React.Component {
     render = (): React.ReactNode =>
         (
-            <div className="tw-relative top-40 tw-text-center tw-bg-green-500">
-                <form className="tw-text-center">
+            <div className="tw-relative tw-top-40 tw-text-center tw-item-center tw-bg-grey">
+                <form className="tw-bg-primary tw-mx-[30rem] tw-py-20">
+                <h1 className="tw-text-xl"> Class Code</h1>
                     <input type="text" name="classcode"/> <br/>
                     <input type="submit"/>
                 </form>
