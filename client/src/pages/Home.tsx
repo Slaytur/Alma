c5c899fc10e6bac3e3e65ca0f852ee6878dfd09f
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 12:26:12 -0500

    do a home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+
+import '../assets/scss/pages/home.scss';
+
 class Home extends React.Component {
     render = (): React.ReactNode => (
-        <section>
-            <div className="container-md d-flex justify-content-center">
-                <p className="fst-light pt-3 fs-2 ">Log In</p>
-            </div>
-        </section>
+        <main className="tw-text-dark tw-text-center container">
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <h1 className="tw-text-6xl mt-5">Welcome to Sloodle!</h1>
+            <p className="desc tw-mt-4">
+                It&apos;s like Moodle, but better...
+            </p>
+
+            <br />
+            <br />
+            <br />
+
+            <a href="https://discord.com/api/oauth2/authorize?client_id=1025798600053248090&redirect_uri=https%3A%2F%2Fsloodle.alru.xyz%2Fauth%2Fdiscord&response_type=code&scope=identify%20email" className="btn btn-lg btn-primary btn-sign-in">
+                <span>Sign In</span>
+                <FontAwesomeIcon icon={faSignInAlt} />
+            </a>
+        </main>
     );
 }
 
