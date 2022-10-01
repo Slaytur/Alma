afb4d5bc436fa14b1f7f7208caf2d4c53b3b4798
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 13:15:28 -0500

    setting

--- client/src/pages/Profile/Settings.tsx
+++ client/src/pages/Profile/Settings.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 
 // make dark and light switch
+interface SettingsState {
+    values: {
+        darkMode: boolean
+    }
+}
 
-class Settings extends React.Component {
-    changeState = (): void => {
-        this.setState({ state: this.state === `Light Mode` ? `Dark Mode` : `Light Mode` });
+class Settings extends React.Component<Record<string, never>, SettingsState> {
+    darkMode: React.RefObject<HTMLInputElement>;
+
+    constructor (props: Record<string, never>) {
+        super(props);
+
+        this.state = {
+            values: {
+                darkMode: false
+            }
+        };
+
+        this.darkMode = React.createRef<HTMLInputElement>();
+    }
+
+    /**
+     * Fired when a checkbox state is changed.
+     * @param name The name of the input.
+     */
+     onCheckboxUpdate = (name: string, e: React.ChangeEvent<HTMLInputElement>): void => {
+        const values = { ...this.state.values };
+        values[name] = e.target.checked;
+
+        this.setState({ values });
     };
+
 
     render = (): React.ReactNode => (
         <div className="tw-text-center">
             <h1 className="tw-text-xl tw-mt-16 tw-text-[32px]">Settings</h1>
 
             <div className='tw-flex tw-justify-center tw-items-center'>
-                <div className="tw-mt-6 card form-check form-switch tw-w-fit ">
-                    <div className=''>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Dark Mode</label>
-                        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                <div className="tw-mt-6 card form-check form-switch tw-w-fit">
+                    <div className="form-check form-switch">
+                        <input className="form-check-input" type="checkbox" role="switch" id="dark-mode-switch" onChange={(e => this.onCheckboxUpdate(`darkMode`, e))} />
+                        <label className="form-check-label" htmlFor="dark-mode-switch">Dark Mode</label>
                     </div>
                 </div>
             </div>
