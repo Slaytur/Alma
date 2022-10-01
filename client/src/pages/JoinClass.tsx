6142e25cd3997fab0d7d9717de043a0d1f89d34e
Sieradni <michaelandbrother@gmail.com>
2022-10-01 14:37:50 -0500

    Update JoinClass.tsx

--- client/src/pages/JoinClass.tsx
+++ client/src/pages/JoinClass.tsx
@@ -4,10 +4,10 @@ class JoinClass extends React.Component {
     render = (): React.ReactNode =>
         (
             <div className="tw-relative tw-top-40 tw-text-center tw-item-center tw-bg-grey">
-                <form className="tw-bg-primary tw-mx-[50rem] tw-py-25">
-                <h1 className="tw-text-xl"> Class Code</h1>
+                <form className="tw-bg-primary tw-mx-[50rem] tw-py-25 tw-rounded-lg">
+                    <h1 className="tw-text-xl tw-py-1"> Class Code</h1>
                     <input type="text" name="classcode"/> <br/>
-                    <input type="submit"/>
+                    <input className="tw-py-1" type="submit"/>
                 </form>
             </div>
         );
