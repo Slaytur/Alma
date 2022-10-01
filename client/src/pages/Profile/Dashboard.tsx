0703e127b16db565824799014a9b7a42c6abb581
charlesbae <90109397+charlesbae@users.noreply.github.com>
2022-10-01 08:55:37 -0500

    fix: profile pages

--- client/src/pages/Profile/Dashboard.tsx
+++ client/src/pages/Profile/Dashboard.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Content from '../components/Content';
-import Lecture from '../components/Lecture';
-import Sidebar from '../components/Sidebar';
-import Assignment from "../components/Assignment";
+import Content from '../../components/Content';
+import Lecture from '../../components/Lecture';
+import Sidebar from '../../components/Sidebar';
+import Assignment from "../../components/Assignment";
 
 class Dashboard extends React.Component {
     render = (): React.ReactNode => (
