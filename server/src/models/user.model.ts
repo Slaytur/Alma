848e8721150b23bfcca18b4cb30da22a052910af
Damien Vesper <ldamienvesper@gmail.com>
2022-10-01 02:27:49 -0500

    init user document

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,28 @@
+import Mongoose from 'mongoose';
+
+interface UserDoc extends Mongoose.Document {
+    created: Date
+
+    username: string
+    email: string
+    avatar?: string
+
+    IP: string
+}
+
+const UserSchema = new Mongoose.Schema({
+    created: { type: Date, required: true },
+
+    username: { type: String, required: true, maxlength: 32, unique: true },
+    email: { type: String, required: true, unique: true },
+    avatar: { type: String, required: false },
+
+    IP: { type: String, required: false }
+});
+
+const User = Mongoose.model<UserDoc>(`User`, UserSchema);
+
+export {
+    User,
+    UserDoc
+};
